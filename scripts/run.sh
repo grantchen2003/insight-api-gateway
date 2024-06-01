27e135b8c6424e5281f9d26c3d655688fce979b6
Productivity <grantchen2021@gmail.com>
2024-06-01 00:25:48 -0400

    updated run.sh to ensure docker desktop is running before starting the container

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,35 @@
+#!/bin/bash
+
+
+is_docker_desktop_running() {
+    if docker info &> /dev/null; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+run_docker_desktop() {
+    echo "starting docker desktop"
+    powershell.exe -Command "& { Start-Process -FilePath 'C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe' -WindowStyle Hidden }"
+}
+
+
+if ! is_docker_desktop_running; then
+    run_docker_desktop
+fi
+
+
+until is_docker_desktop_running; do
+    sleep 2
+done
+
+
+echo "docker desktop is running"
+
 cd ..
 
-docker compose up
+docker build -t insight-api-gateway .
+
+docker run -p 5000:5000 insight-api-gateway
